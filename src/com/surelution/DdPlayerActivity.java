2ccd950d0ddcffa89f80c6c9d246f48dcd0d83be
boyxgz <guangzongsyu@gmail.com>
2012-09-11 09:32:06 +0800

    refactor

--- src/com/surelution/DdPlayerActivity.java
+++ src/com/surelution/DdPlayerActivity.java
@@ -12,7 +12,6 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.util.Xml;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,96 +21,91 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class DdPlayerActivity extends Activity implements OnClickListener {
-//    private MediaPlayer player;
-    private Fragment fragment;
-	
+public class DdPlayerActivity extends Activity {
+
+	private Fragment fragment;
+
 	private Stack<Fragment> stack = new Stack<Fragment>();
 	private Fragment currFragment;
-	
+
 	private String currPlayerPath;
 	private MediaPlayer currPlayer;
 
 	private CountDownTimer timer;
 
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.main);
-        
-        try {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		try {
 			parseXml();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-        
-        currFragment = fragment;
-        redraw();
-    }
 
-	@Override
-	public void onClick(View view) {
-//		if(view.getId() == R.id.button1) {
-//			play(21000, 40000);
-//		}
+		currFragment = fragment;
+		redraw();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main_menu, menu);
-	    return true;
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getItemId() == R.id.menuItemCopyright) {
+		if (item.getItemId() == R.id.menuItemCopyright) {
 			Intent i = new Intent(this, CopyrightActivity.class);
 			startActivity(i);
-		} else if(item.getItemId() == R.id.menuItemExit) {
+		} else if (item.getItemId() == R.id.menuItemExit) {
 			finish();
 			System.exit(0);
 		}
 		return true;
 	}
-	
+
 	private void redraw() {
 		Stack<Fragment> stack = new Stack<Fragment>();
 		stack.push(currFragment);
 		Fragment parent = currFragment.getParent();
-		while(parent != null) {
+		while (parent != null) {
 			stack.push(parent);
 			parent = parent.getParent();
 		}
-        LinearLayout breadCrumb = (LinearLayout)findViewById(R.id.tableRow1);
-        LinearLayout op = (LinearLayout)findViewById(R.id.tableRow2);
-        
-        breadCrumb.removeAllViews();
-        op.removeAllViews();
-        
-        class l implements OnClickListener {
-        	
-        	private Fragment fragment;
+		LinearLayout breadCrumb = (LinearLayout) findViewById(R.id.tableRow1);
+		LinearLayout op = (LinearLayout) findViewById(R.id.tableRow2);
 
-        	private int start, end;
-        	l(Fragment fragment) {
-        		this.fragment = fragment;
-        		if(this.fragment.start == null) {
-        			start = 0;
-        		} else {
-        			start = this.fragment.start;
-        		}
-        		if(this.fragment.end == null) {
-        			end = getPlayer(fragment.getMediaFile()).getDuration();
-        		} else {
-        			end = this.fragment.end;
-        		}
-        	}
+		breadCrumb.removeAllViews();
+		op.removeAllViews();
+
+		class l implements OnClickListener {
+
+			private Fragment fragment;
+
+			private int start, end;
+
+			l(Fragment fragment) {
+				this.fragment = fragment;
+				if (this.fragment.start == null) {
+					start = 0;
+				} else {
+					start = this.fragment.start;
+				}
+				if (this.fragment.end == null) {
+					end = getPlayer(fragment.getMediaFile()).getDuration();
+				} else {
+					end = this.fragment.end;
+				}
+			}
+
 			@Override
 			public void onClick(View v) {
-				if(fragment.getChildren() == null) {
+				if (fragment.getChildren() == null) {
 					play(fragment.start, fragment.end);
 				} else {
 					currFragment = fragment;
@@ -119,42 +113,39 @@ public class DdPlayerActivity extends Activity implements OnClickListener {
 					play(start, end);
 				}
 			}
-        	
-        }
-        
-        while(!stack.empty()) {
-        	Fragment f = stack.pop();
-        	Button b = new Button(this);
-        	b.setText(f.title);
-        	b.setOnClickListener(new l(f));
-        	breadCrumb.addView(b);
-        }
-        
-		for(Fragment f : currFragment.getChildren()) {
-        	Button b = new Button(this);
-        	b.setText(f.title);
-        	b.setOnClickListener(new l(f));
-        	op.addView(b);
-        }
+
+		}
+
+		while (!stack.empty()) {
+			Fragment f = stack.pop();
+			Button b = new Button(this);
+			b.setText(f.title);
+			b.setOnClickListener(new l(f));
+			breadCrumb.addView(b);
+		}
+
+		for (Fragment f : currFragment.getChildren()) {
+			Button b = new Button(this);
+			b.setText(f.title);
+			b.setOnClickListener(new l(f));
+			op.addView(b);
+		}
 	}
-	
+
 	private void play(int start, int end) {
-//		if(getPlayer(fragment.getMediaFile()).isPlaying()) {
-//			player.pause();
-//		}
 		final MediaPlayer player = getPlayer(fragment.getMediaFile());
 		player.seekTo(start);
 		player.start();
-		if(timer != null) {
+		if (timer != null) {
 			timer.cancel();
 		}
 		timer = new CountDownTimer(end - start, 10) {
-			
+
 			@Override
 			public void onTick(long millisUntilFinished) {
-				
+
 			}
-			
+
 			@Override
 			public void onFinish() {
 				player.pause();
@@ -167,47 +158,48 @@ public class DdPlayerActivity extends Activity implements OnClickListener {
 		String xmlPath = "/sdcard/letaotao/fragment.xml";
 		InputStream in = new FileInputStream(xmlPath);
 		XmlPullParser parser = Xml.newPullParser();
-        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-        parser.setInput(in, null);
-        int event = parser.next();
-        while(event != XmlPullParser.END_DOCUMENT) {
-        	if(event == XmlPullParser.START_TAG) {
-                String name = parser.getName();
-                String path = parser.getAttributeValue(null, "path");
-                String description = parser.getAttributeValue(null, "description");
-                String title = parser.getAttributeValue(null, "title");
-                String sStart = parser.getAttributeValue(null, "start");
-                String sEnd = parser.getAttributeValue(null, "end");
-                Integer start = null;
-                Integer end = null;
-                if(sStart != null) {
-                	start = Integer.parseInt(sStart);
-                }
-                if(sEnd != null) {
-                	end = Integer.parseInt(sEnd);
-                }
-                Log.i("shit", name + "," + "," + path + "," + description + "," + title + "," + start + "," + end);
-                Fragment fragment = new Fragment(title, path, description, start, end);
-                stack.push(fragment);
-        	} else if(event == XmlPullParser.END_TAG) {
-        		fragment = stack.pop();
-        		if(!stack.isEmpty()) {
-            		Fragment top = stack.peek();
-            		if(top != null) {
-            			top.addChild(fragment);
-            		}
-        		}
-        	}
-        	event = parser.next();
-        }
-        
+		parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+		parser.setInput(in, null);
+		int event = parser.next();
+		while (event != XmlPullParser.END_DOCUMENT) {
+			if (event == XmlPullParser.START_TAG) {
+//				String name = parser.getName();
+				String path = parser.getAttributeValue(null, "path");
+				String description = parser.getAttributeValue(null,
+						"description");
+				String title = parser.getAttributeValue(null, "title");
+				String sStart = parser.getAttributeValue(null, "start");
+				String sEnd = parser.getAttributeValue(null, "end");
+				Integer start = null;
+				Integer end = null;
+				if (sStart != null) {
+					start = Integer.parseInt(sStart);
+				}
+				if (sEnd != null) {
+					end = Integer.parseInt(sEnd);
+				}
+				Fragment fragment = 
+						new Fragment(title, path, description, start, end);
+				stack.push(fragment);
+			} else if (event == XmlPullParser.END_TAG) {
+				fragment = stack.pop();
+				if (!stack.isEmpty()) {
+					Fragment top = stack.peek();
+					if (top != null) {
+						top.addChild(fragment);
+					}
+				}
+			}
+			event = parser.next();
+		}
+
 	}
-	
+
 	private MediaPlayer getPlayer(String path) {
-		if(!path.equals(currPlayerPath)) {
+		if (!path.equals(currPlayerPath)) {
 			currPlayerPath = path;
-	        Uri uri = Uri.parse("file:///sdcard/letaotao/" + path);
-	        currPlayer = MediaPlayer.create(this, uri);
+			Uri uri = Uri.parse("file:///sdcard/letaotao/" + path);
+			currPlayer = MediaPlayer.create(this, uri);
 		}
 		return currPlayer;
 	}
